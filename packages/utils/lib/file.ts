60027480647cd6c8fade1afaf181739f18a78b30
zksu <565338107@qq.com>
2024-02-21 21:36:14 +0800

    feat: add utils

--- packages/utils/lib/file.ts
+++ packages/utils/lib/file.ts
@@ -1,10 +1,32 @@
-import fs from "fs";
-import util from "util";
-export async function checkFileExists(filename: string) {
-  return !(await util
-    .promisify(fs.access)(filename, fs.constants.F_OK)
-    .catch(() => {
-      return true;
-    }));
+import fs from "node:fs";
+
+/**
+ * 说明: 检查文件是否存在
+ * @param filePath 文件路径
+ * @returns 
+ * @example
+ * const result = await getFileStat("./a.txt") // -> 
+ */
+export async function getFileStat(filePath: string) {
+  return new Promise((resolve) => {
+    fs.stat(filePath, (err: any, stats: any) => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(stats);
+      }
+    });
+  });
 }
 
+/**
+ * 说明: 拷贝文件
+ * @param src 源地址
+ * @param dist 目的地地址
+ * @returns
+ * @example
+ * copy("./a.txt","./b.txt") // ->
+ */
+export function copy(src: string, dist: string) {
+  return fs.createReadStream(src).pipe(fs.createWriteStream(dist));
+}
