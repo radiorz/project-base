0b9450391c845adbf29be2cd03c63baca0aa331f
zksu <565338107@qq.com>
2024-02-27 09:39:09 +0800

    chore: 一些小变化

--- apps/server-template/src/app.module.ts
+++ apps/server-template/src/app.module.ts
@@ -4,7 +4,15 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User as UserEntity } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
-export const DbModule = TypeOrmModule.forRoot({
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const GlobalConfigModule = ConfigModule.forRoot({
+  // 指定存储环境变量的文件, 靠前的文件拥有较高的优先级
+  envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
+  isGlobal: true,
+}); // 全局导入 ConfigModule
+
+/* export const DbModule = TypeOrmModule.forRoot({
   type: 'mysql',
   host: 'localhost',
   port: 3306,
@@ -13,10 +21,15 @@ export const DbModule = TypeOrmModule.forRoot({
   database: 'test',
   entities: [UserEntity],
   synchronize: true,
-});
+}); */
+
 @Module({
-  imports: [DbModule, UserModule],
+  imports: [GlobalConfigModule /* , DbModule */, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {
+    console.log(`this.configService.get('TOKEN_SECRET');`, this.configService);
+  }
+}
--- apps/server-template/src/main.ts
+++ apps/server-template/src/main.ts
@@ -2,11 +2,38 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import appConfig from './config/app.config';
 import { configSwagger } from './config/swagger.config';
+import helmet from 'helmet';
+
+function applyHelmet(app) {
+  // 防止 @apollo/server等不行用
+  const options = {
+    crossOriginEmbedderPolicy: false,
+    contentSecurityPolicy: {
+      directives: {
+        imgSrc: [
+          `'self'`,
+          'data:',
+          'apollo-server-landing-page.cdn.apollographql.com',
+        ],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+        manifestSrc: [
+          `'self'`,
+          'apollo-server-landing-page.cdn.apollographql.com',
+        ],
+        frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+      },
+    },
+  };
+  app.use(helmet(options));
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, appConfig);
   // api 开头
   app.setGlobalPrefix('api');
   configSwagger(app);
+  applyHelmet(app);
   await app.listen(3000);
 }
+
 bootstrap();
