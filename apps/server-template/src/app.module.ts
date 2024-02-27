0fbc6e529bc1e4adb8590a77651e3f74596c9edd
zksu <565338107@qq.com>
2024-02-27 11:59:02 +0800

    feat: db is ok

--- apps/server-template/src/app.module.ts
+++ apps/server-template/src/app.module.ts
@@ -1,35 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User as UserEntity } from './user/entities/user.entity';
+import { DbModule } from './config/db.config';
 import { UserModule } from './user/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
-export const GlobalConfigModule = ConfigModule.forRoot({
-  // 指定存储环境变量的文件, 靠前的文件拥有较高的优先级
-  envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
-  isGlobal: true,
-}); // 全局导入 ConfigModule
-
-/* export const DbModule = TypeOrmModule.forRoot({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'test',
-  entities: [UserEntity],
-  synchronize: true,
-}); */
-
+import { EnvConfigModule } from './config/env.config';
 @Module({
-  imports: [GlobalConfigModule /* , DbModule */, UserModule],
+  imports: [EnvConfigModule, DbModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  constructor(private readonly configService: ConfigService) {
-    console.log(`this.configService.get('TOKEN_SECRET');`, this.configService);
-  }
+  constructor() {}
 }
