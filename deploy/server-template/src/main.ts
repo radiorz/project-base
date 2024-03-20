e02500969692997879c23e670b57977d9fb459a9
zksu <565338107@qq.com>
2024-03-20 14:03:30 +0800

    feat: 调整目录结构

--- deploy/server-template/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { hello } from 'common';
-console.log(`hello`, hello);
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
