a511a1fb73e2bb0ed5c84f3de74f16d35a53b724
tikkhun <s565338107@163.com>
2024-06-19 15:47:14 +0800

    chore(create-tsup): 增加特性。

--- templates/create-tsup/bin/create-tsup.js
+++ templates/create-tsup/bin/create-tsup.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 import inquirer from 'inquirer';
 import { join } from 'path';
 import { fileURLToPath } from 'node:url';
+import templatePackageJson from '../template/package.json' assert { type: 'json' };
 const prompts = [
   {
     type: 'input',
@@ -18,6 +19,12 @@ const prompts = [
   // },
 ];
 async function bootstrap() {
+  if (parseInt(process.version.slice(1)) < 16) {
+    console.error('你的 Node.js 版本过低，请升级到 Node.js 12 或更高版本。');
+    process.exit(1);
+  } else {
+    console.log('Node.js 版本符合要求！');
+  }
   const { name } = await inquirer.prompt(prompts);
   console.log(`target name:`, name);
   const source = join(fileURLToPath(new URL('.', import.meta.url)), '../template');
@@ -26,6 +33,9 @@ async function bootstrap() {
     if (err) {
       console.log(`err`, err);
     }
+    // 替换package.json里面的名称
+    templatePackageJson.name = name;
+    fs.writeFileSync(join(name, 'package.json'), JSON.stringify(templatePackageJson, null, 2));
   });
 }
 bootstrap();
