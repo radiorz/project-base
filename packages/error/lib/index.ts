b100a59764a8f305460d55727edd3c4e5fe21465
tikkhun <s565338107@163.com>
2024-07-04 13:44:21 +0800

    test(error): it works good.

--- packages/error/lib/index.ts
+++ packages/error/lib/index.ts
@@ -6,21 +6,23 @@ const { merge } = _;
 interface TkErrorOptions {
   message: string;
   code: string | number;
-  // i18n: boolean;
+  i18n: boolean;
   i18nAdapter: (message: string) => string;
 }
 const defaultOptions: TkErrorOptions = {
   message: '',
   code: 1,
-  // i18n: false,
+  i18n: false,
   i18nAdapter: (message: string) => message, // 等于5️无
 };
 export class TkError extends Error {
   opts: TkErrorOptions = defaultOptions;
   constructor(options: Partial<TkErrorOptions>) {
-    const message = options.message || defaultOptions.message;
-    const i18nAdapter = options.i18nAdapter || defaultOptions.i18nAdapter;
-    const finalMessage = i18nAdapter(message);
+    let finalMessage = options.message || defaultOptions.message;
+    if (options.i18n) {
+      const i18nAdapter = options.i18nAdapter || defaultOptions.i18nAdapter;
+      finalMessage = i18nAdapter(finalMessage);
+    }
     super(finalMessage);
     this.opts = merge(this.opts, options);
   }
