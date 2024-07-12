d02c19479d91201c77d3097e5ae9c7356c471a60
tikkhun <s565338107@163.com>
2024-07-12 10:51:09 +0800

    chore: platform

--- packages/utils/lib/platform.ts
+++ packages/utils/lib/platform.ts
@@ -2,6 +2,12 @@
 export function isClient() {
   return typeof window !== 'undefined' && typeof document !== 'undefined';
 }
+export function isClientWindows() {
+  return window.navigator.platform === 'Win32';
+}
+export function isAndroid() {
+  throw new Error('impl not ready');
+}
 // nodejs commanjs 环境
 export function isCommonNode() {
   return typeof module !== 'undefined' && module.exports;
