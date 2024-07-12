3ec4ec2be0365954dcf1362159d2c5abf0ecbf32
tikkhun <s565338107@163.com>
2024-07-12 10:44:22 +0800

    chore: isClient

--- packages/utils/lib/platform.ts
+++ packages/utils/lib/platform.ts
@@ -1,17 +1,18 @@
-export function isWeb() {
-  return typeof window !== 'undefined';
+// isWeb
+export function isClient() {
+  return typeof window !== 'undefined' && typeof document !== 'undefined';
 }
 // nodejs commanjs 环境
 export function isCommonNode() {
   return typeof module !== 'undefined' && module.exports;
 }
 
-export function isWindows() {
-  return process.platform === 'win32';
+export function isPowershell() {
+  throw new Error('impl not ready');
 }
 
-export function isPowershell() {
-  
+export function isWindows() {
+  return process.platform === 'win32';
 }
 export function isLinux() {
   return process.platform === 'linux';
