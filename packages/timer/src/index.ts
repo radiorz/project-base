f7d71e54e08f1823f6264bd0716ac55189187b06
tikkhun <s565338107@163.com>
2024-07-15 16:12:31 +0800

    chore(timer): test seems well.

--- packages/timer/src/index.ts
+++ packages/timer/src/index.ts
@@ -3,7 +3,7 @@ const ticker = new Ticker();
 const timer = new Timer({
   isOnTime: 'second',
   onTime: (now) => {
-    console.log(`timer1`, now);
+    console.log(timer.id, '111', now);
   },
 });
 
@@ -11,28 +11,37 @@ const timer2 = new Timer({
   ticker,
   isOnTime: 'second',
   onTime: (now) => {
-    console.log(`timer2`, now);
+    console.log(timer2.id, '222', now);
   },
 });
 const timer3 = new Timer({
   ticker,
   isOnTime: 'second',
   onTime: (now) => {
-    console.log(`timer3`, now);
+    console.log(timer3.id, '333', now);
   },
 });
 timer.init(ticker).start();
-// setTimeout(() => {
-//   timer2.stop();
-// }, 3000);
-// setTimeout(() => {
-//   timer.stop();
-// }, 2000);
-// setTimeout(() => {
-//   timer3.stop();
-// }, 4000);
-
 setTimeout(() => {
-  ticker.stop();
-  ticker.start();
-}, 5000);
+  timer2.stop();
+}, 3000);
+setTimeout(() => {
+  timer.stop();
+}, 2000);
+setTimeout(() => {
+  timer3.stop();
+}, 4000);
+
+// setTimeout(() => {
+//   // ticker.stop();
+//   ticker.start();
+//   const timer = new Timer({
+//     ticker: ticker,
+//     onTime(now) {
+//       console.log(`now`, now);
+//     },
+//   });
+//   setTimeout(() => {
+//     timer.stop();
+//   }, 1000);
+// }, 5000);
