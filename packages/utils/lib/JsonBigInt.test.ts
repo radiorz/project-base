ae4d4aab9d49393f826261e411eaf2bc702cf341
tikkhun <s565338107@163.com>
2024-07-18 15:07:39 +0800

    chore(utils): is good.

--- packages/utils/lib/JsonBigInt.test.ts
+++ packages/utils/lib/JsonBigInt.test.ts
@@ -3,9 +3,10 @@ import { DEFAULT_JSON_BIGINT_OPTIONS, JsonBigInt } from './JsonBigInt';
 
 it('jsonbigint is work', () => {
   const jsonBigInt = new JsonBigInt();
-  const str = jsonBigInt.stringify({
+  const origin = {
     a: 132n,
-  });
+  };
+  const str = jsonBigInt.stringify(origin);
   expect(str).toBe(
     JSON.stringify({
       a: `${DEFAULT_JSON_BIGINT_OPTIONS.magicString}132${DEFAULT_JSON_BIGINT_OPTIONS.magicString}`,
@@ -14,4 +15,5 @@ it('jsonbigint is work', () => {
   const json = jsonBigInt.parse(str);
   console.log(`json`, json);
   expect(typeof json.a).toBe('bigint');
+  expect(json.a).toBe(origin.a);
 });
---
+++ packages/utils/lib/json.test.ts
@@ -0,0 +1,56 @@
+import { expect, describe, test } from 'vitest';
+import { jsonToList, ListItem, listToJson } from './json';
+
+describe('listToJson', () => {
+  test('converts list to JSON object', () => {
+    const list: ListItem[] = [
+      { key: 'foo.bar', value: 1 },
+      { key: 'foo.baz', value: 2 },
+      { key: 'qux', value: 3 },
+    ];
+
+    const expectedJson = {
+      foo: {
+        bar: 1,
+        baz: 2,
+      },
+    };
+
+    const options = {
+      delimiter: '.',
+      list,
+      isKeyInclude: (key: string) => key.startsWith('foo'),
+      keyItemTransformer: (key: string) => key.toLowerCase(),
+    };
+
+    const result = listToJson(options);
+
+    expect(result).toStrictEqual(expectedJson);
+  });
+});
+
+describe('jsonToList', () => {
+  test('converts JSON object to list', () => {
+    const json = {
+      foo: {
+        bar: 1,
+        baz: 2,
+      },
+      qux: 3,
+    };
+
+    const expectedList: ListItem[] = [
+      { key: 'foo.bar', value: 1 },
+      { key: 'foo.baz', value: 2 },
+      { key: 'qux', value: 3 },
+    ];
+
+    const options = {
+      delimiter: '.',
+      json,
+    };
+
+    const result = jsonToList(options);
+    expect(result).toStrictEqual(expectedList);
+  });
+});
