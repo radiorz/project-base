188d0e4e2f7236fe639b7c5fdf96a32a807ca372
tikkhun <s565338107@163.com>
2024-08-01 11:22:52 +0800

    chore(requestable): faker the url

--- packages/requestable/lib/index.test.ts
+++ packages/requestable/lib/index.test.ts
@@ -22,17 +22,17 @@ it('request response work', async () => {
   function handler(data: any) {
     return data;
   }
-  responsive.addRoute('hello', handler);
+  const url = faker.string.uuid();
+  responsive.addRoute(url, handler);
   responsive.init();
   const requestable = new Requestable({
     emitter: emitter,
   });
   requestable.init();
   const requestMessage = {
-    url: 'hello',
+    url: url,
     payload: { name: faker.string.uuid() },
   };
-  console.log(`payload`, requestMessage);
   const result = await requestable.request(requestMessage);
   const handlerResult = await handler(requestMessage.payload);
   expect(result.payload).toBe(handlerResult);
