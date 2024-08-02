354c16e5b5515b9acf07d602ac271960c4b82f02
tikkhun <s565338107@163.com>
2024-08-02 14:39:48 +0800

    chore: update.

--- packages/utils/lib/object.ts
+++ packages/utils/lib/object.ts
@@ -25,3 +25,18 @@ export function isPlainObject(obj: any) {
 export function clone(obj: any) {
   return JSON.parse(JSON.stringify(obj));
 }
+/**
+ * 调整key顺序
+ * @param obj
+ * @returns
+ */
+export function sortKey(obj: Record<string, any>) {
+  const sortedObj = Object.keys(obj)
+    .sort()
+    .reduce((sorted: any, key) => {
+      sorted[key] = obj[key];
+      return sorted;
+    }, {});
+
+  return sortedObj;
+}
