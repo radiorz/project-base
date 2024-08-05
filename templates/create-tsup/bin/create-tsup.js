e3002aeebc8323992cd4f5a1aa24e22f3563d58d
tikkhun <s565338107@163.com>
2024-08-05 23:09:48 +0800

    chore(template/tsup): add cli template

--- templates/create-tsup/bin/create-tsup.js
+++ templates/create-tsup/bin/create-tsup.js
@@ -10,12 +10,12 @@ const prompts = [
     name: 'projectName',
     message: '项目名称',
   },
-  // {
-  //   type: 'list',
-  //   name: 'lang',
-  //   message: 'JS/TS',
-  //   choices: ['JavaScript', 'TypeScript'],
-  // },
+  {
+    type: 'list',
+    name: 'template',
+    message: '模板名称',
+    choices: ['lib', 'cli'],
+  },
 ];
 async function bootstrap() {
   checkNodeVersion(12);
@@ -23,7 +23,7 @@ async function bootstrap() {
   let options = await inquirer.prompt(prompts);
   const creator = new Creator({
     ...options,
-    template: join(dirname(fileURLToPath(import.meta.url)), '../template'),
+    template: join(dirname(fileURLToPath(import.meta.url)), '../templates'),
     templateFiles: ['package.json', 'README.md'],
   });
   await creator.start();
