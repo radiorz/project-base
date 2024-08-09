da0195baaf575fc83686de81e86418ccc3d535a7
tikkhun <s565338107@163.com>
2024-08-09 16:26:46 +0800

    chore: cli-utils is work.

--- tools/create-tools/lib/cli.ts
+++ tools/create-tools/lib/cli.ts
@@ -7,32 +7,21 @@ echoPackage();
 const stringDefaultOptions = OptionHandler.toString(DEFAULT_OPTIONS);
 program
   .description('打包')
-  .option('--template <template>', '模板路径', DEFAULT_OPTIONS.template)
-  .option('--templateExclude <templateExclude>', '不包含的一些文件', DEFAULT_OPTIONS.templateExclude.toString())
-  .option('--projectName <projectName>', '项目名', DEFAULT_OPTIONS.projectName)
-  .option('--workspace <workspace>', '根路径', DEFAULT_OPTIONS.workspace)
-  .option('--templateFiles <templateFiles>', '需模板替换的文件', DEFAULT_OPTIONS.templateFiles.toString())
-  .option('--replaces <replaces>', '替换名称的路径', '')
+  .option('--template <template>', '模板路径', stringDefaultOptions.template)
+  .option('--templateExclude <templateExclude>', '不包含的一些文件', stringDefaultOptions.templateExclude)
+  .option('--projectName <projectName>', '项目名', stringDefaultOptions.projectName)
+  .option('--workspace <workspace>', '根路径', stringDefaultOptions.workspace)
+  .option('--templateFiles <templateFiles>', '需模板替换的文件', stringDefaultOptions.templateFiles)
+  .option('--replaces <replaces>', '替换名称的路径', stringDefaultOptions.replaces)
   .action(async (options: any) => {
-    const opts = {
-      ...options,
-      templateExclude: options.templateExclude.split(','),
-      templateFiles: options.templateFiles.split(','),
-      replaces:
-        typeof options.replaces === 'string'
-          ? options.replaces.split(',').map((str: string) => {
-              if (!str) return;
-              const [sourcePath, targetPath] = str.split('=');
-              if (!sourcePath || !targetPath) {
-                return;
-              }
-              return {
-                sourcePath,
-                targetPath,
-              };
-            })
-          : [],
-    };
+    const opts = OptionHandler.toType(options, {
+      template: 'string',
+      projectName: 'string',
+      workspace: 'string',
+      templateExclude: 'array',
+      templateFiles: 'array',
+      replaces: 'keyValueArray',
+    });
     const inst = new Creator(opts);
     await inst.start();
   });
