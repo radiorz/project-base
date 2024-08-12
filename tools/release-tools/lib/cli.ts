18c9764d5efbe515af6139edd733583a6fd91c70
tikkhun <s565338107@163.com>
2024-08-12 10:39:27 +0800

    chore: use cli-utils to handle options.

--- tools/release-tools/lib/cli.ts
+++ tools/release-tools/lib/cli.ts
@@ -3,29 +3,33 @@ import { program } from 'commander';
 import { Logger } from '@tikkhun/logger';
 import { Release, DEFAULT_OPTIONS } from './Release';
 import packageJson from '../package.json';
+import { OptionHandler } from '@tikkhun/cli-utils';
 const name = packageJson.name;
 Logger.log(`[欢迎使用] ${name}`);
-
+const stringDefaultOptions = OptionHandler.toString(DEFAULT_OPTIONS);
 program
   .description('打包')
-  .option('--workspace <workspace>', '根路径', DEFAULT_OPTIONS.workspace)
-  .option('--include <include>', '包含', DEFAULT_OPTIONS.include.toString())
-  .option('--exclude <exclude>', '不包含', DEFAULT_OPTIONS.exclude.toString())
-  .option('--releaseName <releaseName>', '打包名称', DEFAULT_OPTIONS.releaseName)
-  .option('--releasePath <releasePath>', '打包存放位置', DEFAULT_OPTIONS.releasePath)
-  .option('--withVersion <withVersion>', '需要版本号', '' + DEFAULT_OPTIONS.withVersion)
-  .option('--withTime <withTime>', '需要打包时间', '' + DEFAULT_OPTIONS.withTime)
-  .option('--timePattern <timePattern>', '版本号样式', DEFAULT_OPTIONS.timePattern)
-  .option('--archiveType <archiveType>', '打包格式', DEFAULT_OPTIONS.archiveType)
+  .option('--workspace <workspace>', '根路径', stringDefaultOptions.workspace)
+  .option('--include <include>', '包含', stringDefaultOptions.include)
+  .option('--exclude <exclude>', '不包含', stringDefaultOptions.exclude)
+  .option('--releaseName <releaseName>', '打包名称', stringDefaultOptions.releaseName)
+  .option('--releasePath <releasePath>', '打包存放位置', stringDefaultOptions.releasePath)
+  .option('--withVersion <withVersion>', '需要版本号', stringDefaultOptions.withVersion)
+  .option('--withTime <withTime>', '需要打包时间', stringDefaultOptions.withTime)
+  .option('--timePattern <timePattern>', '版本号样式', stringDefaultOptions.timePattern)
+  .option('--archiveType <archiveType>', '打包格式', stringDefaultOptions.archiveType)
   .action(async (options: any) => {
-    const { include, exclude, withVersion, withTime } = options;
-    const opts = {
-      ...options,
-      include: include.split(','),
-      exclude: exclude.split(','),
-      withVersion: withVersion === 'true',
-      withTime: withTime === 'true',
-    };
+    const opts = OptionHandler.toType(options, {
+      workspace: 'string',
+      include: 'array',
+      exclude: 'array',
+      releaseName: 'string',
+      releasePath: 'string',
+      withVersion: 'boolean',
+      withTime: 'boolean',
+      timePattern: 'string',
+      archiveType: 'string',
+    });
     const release = new Release(opts);
     await release.start();
   });
--- tools/release-tools/tsup.config.ts
+++ tools/release-tools/tsup.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'tsup';
-
+import packageJson from './package.json';
+const name = packageJson.name;
+const version = packageJson.version;
 export default defineConfig((options) => ({
   // 入口文件
   // 不直接指定index 是为了支持直接使用某个文件
@@ -17,7 +19,8 @@ export default defineConfig((options) => ({
   minify: !options.watch,
   banner: {
     js: `/**
-  @tikkhun/logger
+  ${name}
+  ${version}
 */`,
   },
 }));
