16e5441f12f1993daf3449913990a5d7ef4e6d2b
tikkhun <s565338107@163.com>
2024-08-31 15:09:20 +0800

    chore: bin统一了，其实就只有templates 不同，之后可能可以继续抽象这货，不需要另外搞bin二十直接用tikkhun-create。

--- templates/create-vite/bin/create.mjs
+++ templates/create-vite/bin/create.mjs
@@ -1,29 +1,26 @@
 #!/usr/bin/env node
 
-import { Creator, checkNodeVersion, echoPackage } from '@tikkhun/create';
+import { Creator, checkNodeVersion, echoPackage, getTemplates } from '@tikkhun/create';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-
+import { input, select } from '@inquirer/prompts';
+const templatesDir = join(dirname(fileURLToPath(import.meta.url)), '../templates');
 async function bootstrap() {
   checkNodeVersion(12);
   echoPackage();
   const projectName = await input({ message: '项目名称', default: Creator.DEFAULT_OPTIONS.projectName });
   const template = await select({
     message: '模块名称',
-    choices: [
-      {
-        value: 'lib',
-        // name: '默认',
-      },
-      {
-        value: 'vuetify-lib',
-        // name: 'esmodule 模板',
-      },
-    ],
+    choices: (await getTemplates(templatesDir)).map((filePath) => {
+      return {
+        value: filePath,
+      };
+    }),
   });
   const creator = new Creator({
     projectName,
-    template: join(dirname(fileURLToPath(import.meta.url)), `../templates/${template}`),
+    template: join(templatesDir, template),
+    templateFiles: ['package.json', 'README.md'],
   });
   await creator.start();
 }
