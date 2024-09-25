1a61c2130f91f918441b37fb81008217e1dc03bb
tikkhun <s565338107@163.com>
2024-09-25 10:07:02 +0800

    chore: configcommand支持init config.

--- packages/cli-core/lib/command/ConfigCommand.ts
+++ packages/cli-core/lib/command/ConfigCommand.ts
@@ -1,19 +1,31 @@
-import { flatJson, jsonToList, unflatJson } from '@tikkhun/utils-core';
-import { OptionHandler } from '../OptionHandler';
-import { AbstractCommand, Action } from './command.interface';
-import { Command, createCommand, program } from 'commander';
-import _ from 'lodash';
-import { readJson } from 'fs-extra';
+import { Command, createCommand } from 'commander';
+import { readJson, writeJson } from 'fs-extra';
 import { join } from 'path';
+import { AbstractCommand, Action } from './command.interface';
+import { Logger } from '@tikkhun/logger';
 export class ConfigCommand extends AbstractCommand {
+  static defaultConfigPath = join(process.cwd(), 'release.json');
   program: Command | undefined;
   init(): void {
     // console.log(`this.options.program`, this.options.program);
     this.program = this.options.program?.command('config') || createCommand();
+    // 这里要获取默认选项便于处理
+    this.program
+      ?.command('init')
+      .option('-p --path <string>', '配置文件路径', ConfigCommand.defaultConfigPath)
+      .action(async (options) => {
+        let { path } = options;
+        if (!path) {
+          path = ConfigCommand.defaultConfigPath;
+        }
+        Logger.log('[开始] 初始化配置文件： ' + path);
+        await writeJson(path, this.options.defaultOptions, { spaces: 2 });
+        Logger.log('[完毕] 初始化配置文件： ' + path);
+      });
     this.addOptions();
   }
   private addOptions() {
-    this.program?.option('-c --config <string>', '配置文件', join(process.cwd(), 'release.json'));
+    this.program?.option('-c --config <string>', '配置文件', ConfigCommand.defaultConfigPath);
   }
   private addAction(action: Action) {
     this.program!.action(async ({ config = '' }) => {
