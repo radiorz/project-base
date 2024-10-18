401242fae998a0ff0276460c177c136d89cc00b7
tikkhun <s565338107@163.com>
2024-10-18 14:58:47 +0800

    chore(creator): works。。

--- tools/create-tools/cli/prompts.ts
+++ tools/create-tools/cli/prompts.ts
@@ -2,24 +2,18 @@
 
 import { input, select } from '@inquirer/prompts';
 import { join } from 'path';
-import { checkNodeVersion, Creator, echoPackage, templatesDir } from '../lib';
+import { checkNodeVersion, Creator, echoPackage, getTemplates, templatesDir } from '../lib';
 checkNodeVersion(12);
 echoPackage();
 
 async function bootstrap() {
   const projectName = await input({ message: '项目名称', default: Creator.DEFAULT_OPTIONS.projectName });
+  const templates = await getTemplates(templatesDir);
   const template = await select({
     message: '模块名称',
-    choices: [
-      {
-        value: 'default',
-        // name: '默认',
-      },
-      {
-        value: 'one-mjs',
-        // name: 'esmodule 模板',
-      },
-    ],
+    choices: templates.map((filePath) => ({
+      value: filePath,
+    })),
   });
   const creator = new Creator({ projectName, template: join(templatesDir, template) });
   await creator.start();
