2fa7dcfa2fe6c5d0073bd128ae64dfb1ee2ce791
tikkhun <s565338107@163.com>
2024-10-19 15:43:24 +0800

    chore: add delimiter

--- packages/utils/lib/consts/delimiter.ts
+++ packages/utils/lib/consts/delimiter.ts
@@ -1,3 +1,9 @@
+/**
+ * 将一些常用分隔符放在这里
+ */
 export const UnderlineDelimiter = '_';
 export const DoubleUnderlineDelimiter = '__';
 export const DotDelimiter = '.';
+export const DashDelimiter = '-';
+export const SlashDelimiter = '/';
+export const PipeDelimiter = '|';
