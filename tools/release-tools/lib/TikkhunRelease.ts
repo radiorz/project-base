5cb5983caf8dd16c9fdaf1a780d3d298fd4fbc16
tikkhun <s565338107@163.com>
2024-10-22 15:49:38 +0800

    fix(TikkhunRelease): works.

--- tools/release-tools/lib/TikkhunRelease.ts
+++ tools/release-tools/lib/TikkhunRelease.ts
@@ -1,12 +1,13 @@
 import { optionsMerge } from '@tikkhun/utils-core';
 import { InfoBuilder, InfoBuilderOptions } from './InfoBuilder';
 import { Release, ReleaseOptions } from './Release';
-import { ReleaseInfoStore, ReleaseInfoStoreOptions, ReleaseInfoStorePlugin } from './ReleaseInfoStorePlugin';
+import { ReleaseInfoStoreOptions, ReleaseInfoStorePlugin } from './ReleaseInfoStorePlugin';
 import { ReleaseName, ReleaseNameOptions } from './ReleaseName';
 import _ from 'lodash';
 import { Logger } from '@tikkhun/logger';
 const { omit } = _;
-export interface TikkhunReleaseDefaultOptions extends Omit<ReleaseOptions, 'infoStore' | 'info' | 'releaseName'> {
+export interface TikkhunReleaseDefaultOptions
+  extends Omit<ReleaseOptions, 'infoStore' | 'info' | 'releaseName' | 'plugins'> {
   // 这个主要集中在info的输入与获取方式
   infoBuilderOptions: Partial<InfoBuilderOptions>;
   // 存储info的文件
@@ -19,7 +20,7 @@ export const TikkhunReleaseDefaultOptions = {
   infoBuilderOptions: InfoBuilder.defaultOptions,
   infoStoreOptions: {
     enabled: true,
-    ...omit(ReleaseInfoStore.defaultOptions, ['info']),
+    ...omit(ReleaseInfoStorePlugin.defaultOptions, ['info']),
   },
   releaseNameOptions: omit(ReleaseName.defaultOptions, ['info']),
 };
@@ -38,7 +39,7 @@ export async function TikkhunRelease(options: TikkhunReleaseDefaultOptions) {
   // release 的额外功能通过插件方式实现
   const plugins: any[] = [];
   if (infoStoreOptions?.enabled) {
-    const releaseInfoStorePlugin = new ReleaseInfoStore({ ...infoStoreOptions, info });
+    const releaseInfoStorePlugin = new ReleaseInfoStorePlugin({ ...infoStoreOptions, info });
     plugins.push(releaseInfoStorePlugin);
   }
   const release = new Release({
