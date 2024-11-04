30801730cf4182f3d415f3b81d30776559fdf3c2
tikkhun <s565338107@163.com>
2024-11-04 15:12:25 +0800

    chore: text.

--- packages/utils/lib/text.test.ts
+++ packages/utils/lib/text.test.ts
@@ -2,5 +2,16 @@ import { it, expect } from 'vitest';
 import { replaceParams } from './text';
 
 it('replaceParams', () => {
+  // 一个变量
   expect(replaceParams('{app}', { app: 'hahah' })).toBe('hahah');
+  // 两个变量
+  expect(replaceParams(`{app}/{org}`, { app: 'hahah', org: 'nnn' })).toBe('hahah/nnn');
+  // 一个变量但写两次
+  expect(replaceParams(`{org}/{org}`, { org: 'nnn' })).toBe('nnn/nnn');
+  // 变量没值 keepMatch=false
+  expect(replaceParams(`{org}`, {})).toBe('');
+  expect(replaceParams(`{org}`, { org: null })).toBe('');
+
+  // 变量没值 keepMatch=true
+  expect(replaceParams(`{org}`, {}, { keepMatch: true })).toBe('{org}');
 });
