bef857fe9a8549fb7f371f246059adab99a80225
tikkhun <s565338107@163.com>
2024-12-06 11:27:30 +0800

    chore: update.

--- templates/create-tsup/bin/create-tsup.js
+++ templates/create-tsup/bin/create-tsup.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-import { Creator, checkNodeVersion, echoPackage, getTemplates } from '@tikkhun/create';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-import { input, select } from '@inquirer/prompts';
-const templatesDir = join(dirname(fileURLToPath(import.meta.url)), '../templates');
-
-
+import { Cli } from '@tikkhun/cli-core';
+import { Creator, TemplateChooser, creatorManagerCliOptions } from '@tikkhun/create';
 import { Logger } from '@tikkhun/logger';
-import { readJSON } from 'fs-extra';
-import path from 'path';
-import { Creator, TemplateChooser } from '../lib';
-import { creatorManagerCli } from '../lib/cli/creator-manager-cli';
-creatorManagerCli.start(async ({ templatesDir, ...userCreatorOptions }: any) => {
+import fsExtra from 'fs-extra';
+const { readJSON } = fsExtra;
+import path, { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+const myCreatorManagerCli = new Cli({
+  ...creatorManagerCliOptions,
+  excludeOptions: [...creatorManagerCliOptions.excludeOptions, 'templatesDir'],
+});
+myCreatorManagerCli.start(async ({ ...userCreatorOptions }) => {
+  const templatesDir = join(dirname(fileURLToPath(import.meta.url)), '../templates');
   // 给定的 templatesDir 可以有两种形式，一种是全局依赖（@tikkhun/create-tsup)，一种是github 一种是。。。
   // 目的就是找到 template ，然后让用户选择，然后就进行项目拷贝创建
   const template = await TemplateChooser({ templatesDir });
@@ -20,7 +20,7 @@ creatorManagerCli.start(async ({ templatesDir, ...userCreatorOptions }: any) => 
   try {
     templateOptions = (await readJSON(path.join(template, 'template.json'))) || {};
     Logger.log('模板默认选项', templateOptions);
-  } catch (error: any) {
+  } catch (error) {
     Logger.warn(`读取templateOptions失败,原因为：` + error.message);
   }
   const creator = new Creator({
--- tools/create-tools/lib/cli/creator-manager-cli.ts
+++ tools/create-tools/lib/cli/creator-manager-cli.ts
@@ -1,9 +1,14 @@
-
 import { Cli, CommandTypes } from '@tikkhun/cli-core';
-import { omit } from 'lodash';
-import { Creator } from '..';
+import _ from 'lodash';
+const { omit } = _;
+import { Creator } from '../Creator';
+import { TemplateChooser } from '../TemplateChooser';
 import packageJson from '../../package.json';
-import { creatorCliOptions } from './creator-cli';
+import { creatorCli, creatorCliOptions } from './creator-cli';
+import { templatesDir } from '../utils';
+import { readJSON } from 'fs-extra';
+import path from 'path';
+import { Logger } from '@tikkhun/logger';
 
 export const creatorManagerCliOptions = {
   name: packageJson.name,
@@ -27,3 +32,24 @@ export const creatorManagerCliOptions = {
 // console.log(`creatorManagerCliOptions`, creatorManagerCliOptions);
 export const creatorManagerCli = new Cli(creatorManagerCliOptions);
 
+// 已经带有 templates
+export async function withTemplatesDir(templatesDir: string) {
+  const template = await TemplateChooser({ templatesDir });
+  const creatorCli = new Cli({ ...creatorCliOptions, excludeOptions: ['template'] });
+  let templateOptions: any;
+  try {
+    templateOptions = (await readJSON(path.join(template, 'template.json'))) || {};
+    Logger.log('模板默认选项', templateOptions);
+  } catch (error: any) {
+    Logger.warn(`读取templateOptions失败,原因为：` + error.message);
+  }
+  creatorCli.start(async (options) => {
+    const creator = new Creator({
+      ...options,
+      ...templateOptions,
+      template,
+      // 其他选项怎么办
+    });
+    await creator.start();
+  });
+}
