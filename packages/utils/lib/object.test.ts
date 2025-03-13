0d407c14f137cd53ec4e7135f0f87d7cece27fa8
tikkhun <s565338107@163.com>
2025-03-13 14:36:57 +0800

    chore: update.

--- packages/utils/lib/object.test.ts
+++ packages/utils/lib/object.test.ts
@@ -2,8 +2,20 @@ import { expect, it } from 'vitest';
 import { killUndefined } from './object';
 
 it('killUndefined', () => {
-  const a = { a: undefined, b: 123 };
-  const b = killUndefined(a);
-  expect('a' in b).toBe(false);
-  expect('b' in b).toBe(true);
+  const origin = {
+    a: undefined,
+    b: 123,
+    c: [1, 2, undefined],
+    d: {
+      a: undefined,
+      b: 123,
+    },
+  };
+  const result = killUndefined(origin);
+  expect('a' in result).toBe(false);
+  expect('b' in result).toBe(true);
+  expect('c' in result).toEqual(true);
+  expect(result.c).toEqual([1, 2]);
+  expect('a' in result.d).toBe(false);
+  expect('b' in result.d).toBe(true);
 });
