576635213ee5622e5afbd6d0fb26da680d1b4644
tikkhun <s565338107@163.com>
2025-05-20 11:12:14 +0800

    chore: update.

--- packages/action/lib/index.ts
+++ packages/action/lib/index.ts
@@ -6,9 +6,10 @@ interface ActionMgr extends Map<string, Function> {
 export const DefaultActionMgrOptions = {};
 export type ActionMgrOptions = typeof DefaultActionMgrOptions;
 export const createActionMgr = (options: Partial<ActionMgrOptions> = {}) => {
-  const opts = Object.assign({}, DefaultActionMgrOptions, options);
+  // const opts = Object.assign({}, DefaultActionMgrOptions, options);
   // 显式指定 actionMgr 类型为 ActionMgr
   const actionMgr = new Map<string, Function>() as ActionMgr;
+   // TODO name 能不能用map中的key来提示。
   actionMgr.do = (name: string, ...args: any[]) => {
     const action = actionMgr.get(name);
     if (!action) throw new Error(`Action ${name} not found`);
