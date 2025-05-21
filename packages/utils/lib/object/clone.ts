ad13fe3f85b85d5a299d0786d35ddb9eafdf697e
tikkhun <s565338107@163.com>
2025-05-21 10:58:22 +0800

    chore: update.

--- packages/utils/lib/object/clone.ts
+++ packages/utils/lib/object/clone.ts
@@ -1,3 +1,7 @@
-export function clone(obj: any) {
+// 函数重载声明
+
+// 实现函数，明确 this 类型
+export function clone(this: any, object?: any) {
+  const obj = object ?? this;
   return JSON.parse(JSON.stringify(obj));
 }
