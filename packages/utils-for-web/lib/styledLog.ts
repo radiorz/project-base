033c897a27a071621a636262a5335242bed93791
tikkhun <s565338107@163.com>
2025-05-21 10:16:59 +0800

    chore: update deps.

--- packages/utils-for-web/lib/styledLog.ts
+++ packages/utils-for-web/lib/styledLog.ts
@@ -1,10 +1,9 @@
-import { styleObjectToString } from "./styleObjectToString";
+import { keyToDashCase } from '@tikkhun/utils-core';
 
 export function styledLog(text: string, style: Record<string, any>) {
   // 将 style 对象转换为 CSS 样式字符串
-  const styleString = styleObjectToString(style)
+  const styleString = keyToDashCase(style);
 
   // 使用转换后的样式字符串调用 console.log
   console.log(`%c${text}`, styleString);
 }
-
