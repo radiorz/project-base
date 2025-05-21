5bc8aec04f2783117851c6f8017d2d215cc616df
tikkhun <s565338107@163.com>
2025-05-21 10:34:39 +0800

    chore: update.

--- packages/utils-for-web/lib/styledLog.ts
+++ packages/utils-for-web/lib/styledLog.ts
@@ -1,8 +1,8 @@
-import { keyToDashCase } from '@tikkhun/utils-core';
+import { tranKeyToDashCase } from '@tikkhun/utils-core';
 
 export function styledLog(text: string, style: Record<string, any>) {
   // 将 style 对象转换为 CSS 样式字符串
-  const styleString = keyToDashCase(style);
+  const styleString = tranKeyToDashCase(style);
 
   // 使用转换后的样式字符串调用 console.log
   console.log(`%c${text}`, styleString);
