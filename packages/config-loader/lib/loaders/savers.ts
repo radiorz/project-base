9473ca32b9084122178137aea51c7f059784009e
tikkhun <s565338107@163.com>
2025-05-28 22:36:21 +0800

    chore(config-loader): xml sheet js-like .env works.

--- packages/config-loader/lib/loaders/savers.ts
+++ packages/config-loader/lib/loaders/savers.ts
@@ -4,6 +4,7 @@ import yaml from 'js-yaml';
 // import JSON5 from 'json5';
 // import toml from 'toml';
 import { convertConfigToXml } from './xml';
+import { nestedObjectToList, NestedObjectToListOptions } from '@tikkhun/utils-core';
 export function saveToJson(data: Config, filePath: string) {
   writeFile(filePath, JSON.stringify(data, null, 2));
 }
@@ -12,14 +13,27 @@ export function saveToYaml(data: Config, filePath: string) {
   writeFile(filePath, content);
 }
 
-export function saveToToml(data: Config, filePath: string) {}
-export function saveToEnv(data: Config, filePath: string) {}
+export function saveToToml(data: Config, filePath: string) {
+  throw new Error('尚未实现');
+}
+export function saveToEnv(data: Config, filePath: string, options?: Partial<NestedObjectToListOptions>) {
+  // TODO 这里好像没有对value进行转换？ nested args 可以上场
+  const env = nestedObjectToList({
+    delimiter: '__',
+    json: data,
+    // keyItemTransformer: function (v: string): string {
+    //   return to(v);
+    // },
+    ...options,
+  });
+  writeFile(filePath, env.map((item) => `${item.key}=${item.value}`).join('\n'));
+}
 // export function saveToJs(data: Config, filePath: string) {}
 export function saveToJsLike(data: Config, filePath: string) {
   const content = `export default ${JSON.stringify(data, null, 2)}`;
   writeFile(filePath, content);
 }
-export function saveToXml(data: Config, filePath: string) {
-  const content = convertConfigToXml(data);
+export async function saveToXml(data: Config, filePath: string) {
+  const content = await convertConfigToXml(data);
   writeFile(filePath, content);
 }
--- packages/config-loader/src/index.ts
+++ packages/config-loader/src/index.ts
@@ -3,10 +3,10 @@ import { loadConfig } from '../lib';
 import { saveConfig } from '../lib/save-config';
 
 async function bootstrap() {
-  console.log('package.json', loadConfig('package.json'));
-  console.log('./src/.env', loadConfig('./src/.env'));
-  console.log('./src/.env', loadConfig('./src/.env', { delimiter: '_' })); // 带参数
-  console.log('./src/test.xml', loadConfig('./src/test.xml'));
+  console.log('package.json', await loadConfig('package.json'));
+  console.log('./src/.env', await loadConfig('./src/.env'));
+  console.log('./src/.env', await loadConfig('./src/.env', { delimiter: '_' })); // 带参数
+  console.log('./src/test.xml', await loadConfig('./src/test.xml'));
   console.log('./src/test.js', await loadConfig('./src/test.js'));
   console.log('./src/test.mjs', await loadConfig('./src/test.mjs'));
   console.log('./src/test.ts', await loadConfig('./src/test.ts'));
@@ -14,6 +14,11 @@ async function bootstrap() {
 }
 bootstrap();
 async function bootstrapSaver() {
-  saveConfig({ test: 123 }, './src/test_saver.json');
+  const config = { test: 123 };
+  await saveConfig(config, './src/test_saver.json');
+  await saveConfig(config, './src/.env.saver');
+  await saveConfig(config, './src/test_saver.xml');
+  await saveConfig(config, './src/test_saver.js');
+  await saveConfig(config, './src/test_saver.xlsx');
 }
 bootstrapSaver();
