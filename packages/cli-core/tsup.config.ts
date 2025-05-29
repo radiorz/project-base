41f84997b9fe6938b1fd02527bb7d8d6af8ba60e
tikkhun <s565338107@163.com>
2025-05-29 17:56:36 +0800

    chore: update.

--- packages/cli-core/tsup.config.ts
+++ packages/cli-core/tsup.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'tsup';
 import packageJson from './package.json';
-const name = packageJson.name;
+const { name, version } = packageJson;
 export default defineConfig((options: any) => ({
   // 入口文件
   // 不直接指定index 是为了支持直接使用某个文件
@@ -19,7 +19,7 @@ export default defineConfig((options: any) => ({
   minify: !options.watch,
   banner: {
     js: `/**
-  ${name}
+  ${name}@${version}
 */`,
   },
 }));
--- templates/create-tsdown/templates/react/tsdown.config.ts
+++ templates/create-tsdown/templates/react/tsdown.config.ts
@@ -3,7 +3,7 @@ import packageJson from './package.json';
 export default defineConfig((options: any) => ({
   // 入口文件
   // 不直接指定index 是为了支持直接使用某个文件
-  entry: [`lib/**/*.tsx`, '!**/*.test.tsx'],
+  entry: [`lib/**/*.tsx`, '!**/*.test.ts'],
   // 格式化
   format: ['esm', 'cjs'],
   // 拆分
