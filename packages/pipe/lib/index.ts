436d015814877fd72839ac19675d39640559acba
tikkhun <s565338107@163.com>
2025-05-30 11:23:55 +0800

    chore: update.

--- packages/pipe/lib/index.ts
+++ packages/pipe/lib/index.ts
@@ -1,5 +1,4 @@
 // pipe // 这里的 pipe 是一个函数组合器，接受多个函数作为参数，并返回一个新的函数
-type AnyFunction = (...args: any[]) => any;
 
 // 同步版本的 pipe 函数
 export const pipe =
@@ -10,5 +9,13 @@ export const pipe =
 // 异步版本的 pipe 函数
 export const asyncPipe =
   <T>(...fns: Array<(arg: T) => Promise<T> | T>) =>
-  async (x: T): Promise<T> =>
-    fns.reduce(async (acc, fn) => fn(await acc), x);
+  async (x: T): Promise<T> => {
+    // 初始值
+    let result: T = x;
+    for (const fn of fns) {
+      // 调用函数并等待结果
+      result = await fn(result);
+    }
+    return result;
+  };
+
--- packages/pipe/src/index.ts
+++ packages/pipe/src/index.ts
@@ -1,2 +1,14 @@
-import { hello } from '../lib';
-hello();
+import { asyncPipe } from '../lib';
+
+async function bootstrap() {
+  const result = await asyncPipe(
+    async (x: any) => {
+      const timeout = 1000;
+      await new Promise((resolve) => setTimeout(resolve, timeout));
+      return x + 1;
+    },
+    (x: any) => x + 2,
+  )(1);
+  console.log(`result`, result);
+}
+bootstrap();
