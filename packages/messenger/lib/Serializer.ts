84c995d9054087a32987377aa7ad9820981f4b52
tikkhun <s565338107@163.com>
2025-06-20 17:48:20 +0800

    chore: 添加说明

--- packages/messenger/lib/Serializer.ts
+++ packages/messenger/lib/Serializer.ts
@@ -1,3 +1,6 @@
+// 序列号可能是一系列的事情 但最终结果就是 jsonlike -> buffer|string ,
+// 最经典的就是 jsonstringify parse ,当然如果要压缩可能换成 msgpack5 等 hex一下 然后message 的话会 minify或者treeshake一下,反过来就是 normalize 咯
+// 反序列化 就是 buffer|string -> jsonlike
 export interface Serialize {
   (data: any): string | Buffer;
 }
