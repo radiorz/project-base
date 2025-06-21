3423eb177a1dcfe65a8bf0b607c55f50812dd9ac
tikkhun <s565338107@163.com>
2025-06-21 15:01:30 +0800

    feat(index-generator): 添加调试选项并更新版本号
    - 在CLI选项中添加debug布尔参数用于调试
    - 调用echoPackage显示包信息
    - 将版本号从0.1.0升级到0.1.1

--- tools/index-generator/lib/cli/index.ts
+++ tools/index-generator/lib/cli/index.ts
@@ -1,17 +1,19 @@
-import { Cli, CommandTypes } from '@tikkhun/cli-core';
+import { Cli, CommandTypes, echoPackage } from '@tikkhun/cli-core';
 import packageJson from '../../package.json';
 import { DefaultGenerateIndexOptions, generateIndex } from '..';
 export const defaultOptions = {
   ...DefaultGenerateIndexOptions,
   child: false
 };
+echoPackage(packageJson)
 export const optionTitles = {
   cwd: '路径',
   indexName: '文件名',
   include: '包含',
   exclude: '排除',
   clean: '清除旧的',
-  child: '子目录'
+  child: '子目录',
+  debug: '调试'
 };
 export const optionTypes = {
   cwd: 'string',
@@ -19,7 +21,8 @@ export const optionTypes = {
   include: 'array',
   exclude: 'array',
   clean: 'boolean',
-  child: 'boolean'
+  child: 'boolean',
+  debug: 'boolean'
 };
 export const cliOptions = {
   name: packageJson.name,
