33a63ace4cfba48a51a726c3ec8baefd0816afd7
tikkhun <s565338107@163.com>
2025-06-24 11:05:09 +0800

    chore: update.

--- packages/protocol-core/lib/topic/topic.ts
+++ packages/protocol-core/lib/topic/topic.ts
@@ -1,5 +1,5 @@
 import { Message } from '../message';
-import { params } from '@tikkhun/utils-core';
+import { mergeOptions, replaceParams } from '@tikkhun/utils-core';
 export interface Topic extends Pick<Message, 'type' | 'subType' | 'module' | 'name' | 'tid' | 'from' | 'to'> {
   /**
    * 公司
@@ -15,6 +15,15 @@ export interface Topic extends Pick<Message, 'type' | 'subType' | 'module' | 'na
   attrs?: string; //比如action的子类型熄灯这个类型,配置变更就跟配置完整路径
 }
 
+export const defaultTopic: Omit<Topic, 'type' | 'name' | 'from'> = {
+  tid: 0,
+  company: '*',
+  domain: '*',
+  to: '*',
+};
 export function buildTopic(topic: Topic): string {
-  return params(`/{company}/{domain}/{tid}/{from}/{to}/{type}/{subType}/{module}/{name}/{attrs}`, topic);
+  return replaceParams(
+    `/{company}/{domain}/{tid}/{from}/{to}/{type}/{subType}/{module}/{name}/{attrs}`,
+    mergeOptions(defaultTopic, topic),
+  );
 }
