dc55e4ca90423414ca9c586682b1955af3d6f524
tikkhun <s565338107@163.com>
2025-07-07 14:20:05 +0800

    chore: update.

--- packages/config/config-loader-cli/tsup.config.ts
+++ packages/config/config-loader-cli/tsup.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'tsup';
-import packageJson from './package.json';
-const name = packageJson.name;
+import pkg from './package.json';
+const { name, version } = pkg;
 export default defineConfig((options) => ({
   // 入口文件
   // 不直接指定index 是为了支持直接使用某个文件
@@ -19,6 +19,7 @@ export default defineConfig((options) => ({
   banner: {
     js: `/**
   ${name}
+  ${version}
 */`,
   },
 }));
